9e076910404f0a041e6647f58babbbbdadc38eb8
zhangwenyang <zhangwenyang@geyecloud.com>
2019-12-20 21:57:29 +0800

    重构:将BaseModel提取到lib中

--- models.py
+++ models.py
@@ -1,14 +1,7 @@
 from django.db import models
+from lib.models import BaseModel
 
 # Create your models here.
-
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True)
-
-    class Meta:
-        abstract = True
 
 
 class Good(BaseModel):
